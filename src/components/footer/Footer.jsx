c34dadcf0edfc45b2341ef6cd380b6cae3f5facc
Artem Vasylenko <irbiskronos@gmail.com>
2024-02-18 10:41:47 +0200

    Footer + contact page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,13 @@
+import styles from './footer.module.css'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>Example</div>
+      <div className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur. All rights reserved.
+      </div>
+    </div>
   )
 }
 
